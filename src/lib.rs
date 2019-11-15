00dae38e285da07c815eb3984b5f2a28e79efdf3
Árni Dagur <arni@dagur.eu>
2019-11-15 02:44:02 +0000

    Put tests in their own module

--- src/lib.rs
+++ src/lib.rs
@@ -11,41 +11,40 @@ pub mod simdutf8check_sse;
 pub use simdutf8check_avx::*;
 pub use simdutf8check_sse::*;
 
-#[test]
-fn valid_sequences_sse() {
+#[cfg(test)]
+mod tests {
     use core::convert::TryInto;
 
-    let arr: [libc::c_char; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
-    unsafe {
-        assert_eq!(
-            simdutf8check_sse::validate_utf8_fast(arr.as_ptr(), arr.len().try_into().unwrap()),
-            true
-        );
+    #[test]
+    fn valid_sequences_sse() {
+        let arr: [libc::c_char; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
+        unsafe {
+            assert_eq!(
+                simdutf8check_sse::validate_utf8_fast(arr.as_ptr(), arr.len().try_into().unwrap()),
+                true
+            );
+        }
     }
-}
 
-#[test]
-fn invalid_sequences_sse() {
-    use core::convert::TryInto;
-
-    let arr: [libc::c_char; 8] = [-2, 2, 3, 4, 5, 6, 7, 8];
-    unsafe {
-        assert_eq!(
-            simdutf8check_sse::validate_utf8_fast(arr.as_ptr(), arr.len().try_into().unwrap()),
-            false
-        );
+    #[test]
+    fn invalid_sequences_sse() {
+        let arr: [libc::c_char; 8] = [-2, 2, 3, 4, 5, 6, 7, 8];
+        unsafe {
+            assert_eq!(
+                simdutf8check_sse::validate_utf8_fast(arr.as_ptr(), arr.len().try_into().unwrap()),
+                false
+            );
+        }
     }
-}
 
-#[test]
-fn valid_sequences_avx() {
-    use core::convert::TryInto;
-
-    let arr: [libc::c_char; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11];
-    unsafe {
-        assert_eq!(
-            simdutf8check_avx::validate_utf8_fast_avx(arr.as_ptr(), arr.len().try_into().unwrap()),
-            true
-        );
+    #[test]
+    fn valid_sequences_avx() {
+        let arr: [libc::c_char; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11];
+        unsafe {
+            assert_eq!(
+                simdutf8check_avx::validate_utf8_fast_avx(arr.as_ptr(), arr.len().try_into().unwrap()),
+                true
+            );
+        }
     }
 }
