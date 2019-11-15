5c8bfa6ad494808043112f2db653ff8e5b6e1870
Árni Dagur <arni@dagur.eu>
2019-11-15 02:25:05 +0000

    Enable no_std

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 // lib.rs
 // Copyright (c) 2019 Árni Dagur <arni@dagur.eu> MIT license
 //
+#![no_std]
 #![feature(stdsimd)]
 pub mod simdutf8check_avx;
 pub mod simdutf8check_sse;
@@ -12,7 +13,7 @@ pub use simdutf8check_sse::*;
 
 #[test]
 fn valid_sequences_sse() {
-    use std::convert::TryInto;
+    use core::convert::TryInto;
 
     let arr: [libc::c_char; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
     unsafe {
@@ -24,8 +25,21 @@ fn valid_sequences_sse() {
 }
 
 #[test]
+fn invalid_sequences_sse() {
+    use core::convert::TryInto;
+
+    let arr: [libc::c_char; 8] = [-2, 2, 3, 4, 5, 6, 7, 8];
+    unsafe {
+        assert_eq!(
+            simdutf8check_sse::validate_utf8_fast(arr.as_ptr(), arr.len().try_into().unwrap()),
+            false
+        );
+    }
+}
+
+#[test]
 fn valid_sequences_avx() {
-    use std::convert::TryInto;
+    use core::convert::TryInto;
 
     let arr: [libc::c_char; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11];
     unsafe {
