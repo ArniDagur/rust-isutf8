63e9b2d15ee612556477d040015459067593346a
Árni Dagur <arnidg@protonmail.ch>
2019-11-17 23:51:38 +0000

    Misc cleanup

--- src/range/sse.rs
+++ src/range/sse.rs
@@ -58,8 +58,8 @@ static RANGE_MAX_TABLE: [i8; 16] = [
     0x80i32 as i8,
 ];
 static DF_EE_TABLE: [i8; 16] = [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0];
-static mut EF_FE_TABLE: [i8; 16] = [0, 3, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-/* Return 0 on success, -1 on error */
+static EF_FE_TABLE: [i8; 16] = [0, 3, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 pub fn is_utf8(bytes: &[u8]) -> bool {
     let mut data = bytes.as_ptr();
     let mut len = bytes.len();
@@ -67,21 +67,20 @@ pub fn is_utf8(bytes: &[u8]) -> bool {
         if len >= 32 {
             let mut prev_input = _mm_set1_epi8(0);
             let mut prev_first_len = _mm_set1_epi8(0);
-            let first_len_tbl = _mm_lddqu_si128(FIRST_LEN_TABLE.as_ptr() as *const __m128i);
-            let first_range_tbl = _mm_lddqu_si128(FIRST_RANGE_TABLE.as_ptr() as *const __m128i);
-            let range_min_tbl = _mm_lddqu_si128(RANGE_MIN_TABLE.as_ptr() as *const __m128i);
-            let range_max_tbl = _mm_lddqu_si128(RANGE_MAX_TABLE.as_ptr() as *const __m128i);
-            let df_ee_tbl = _mm_lddqu_si128(DF_EE_TABLE.as_ptr() as *const __m128i);
-            let ef_fe_tbl = _mm_lddqu_si128(EF_FE_TABLE.as_ptr() as *const __m128i);
+            let first_len_table = _mm_lddqu_si128(FIRST_LEN_TABLE.as_ptr() as *const __m128i);
+            let first_range_table = _mm_lddqu_si128(FIRST_RANGE_TABLE.as_ptr() as *const __m128i);
+            let range_min_table = _mm_lddqu_si128(RANGE_MIN_TABLE.as_ptr() as *const __m128i);
+            let range_max_table = _mm_lddqu_si128(RANGE_MAX_TABLE.as_ptr() as *const __m128i);
+            let df_ee_table = _mm_lddqu_si128(DF_EE_TABLE.as_ptr() as *const __m128i);
+            let ef_fe_table = _mm_lddqu_si128(EF_FE_TABLE.as_ptr() as *const __m128i);
             let mut error = _mm_set1_epi8(0);
 
             while len >= 32 {
                 /* **************************** block 1 ****************************/
-                let input: __m128i = _mm_lddqu_si128(data as *const __m128i);
-                let mut high_nibbles: __m128i =
-                    _mm_and_si128(_mm_srli_epi16(input, 4), _mm_set1_epi8(0xF));
-                let first_len: __m128i = _mm_shuffle_epi8(first_len_tbl, high_nibbles);
-                let mut range: __m128i = _mm_shuffle_epi8(first_range_tbl, high_nibbles);
+                let input = _mm_lddqu_si128(data as *const __m128i);
+                let mut high_nibbles = _mm_and_si128(_mm_srli_epi16(input, 4), _mm_set1_epi8(0xF));
+                let first_len = _mm_shuffle_epi8(first_len_table, high_nibbles);
+                let mut range = _mm_shuffle_epi8(first_range_table, high_nibbles);
                 range = _mm_or_si128(range, _mm_alignr_epi8(first_len, prev_first_len, 15));
                 let mut tmp1 = _mm_subs_epu8(first_len, _mm_set1_epi8(1));
                 let mut tmp2 = _mm_subs_epu8(prev_first_len, _mm_set1_epi8(1));
@@ -92,19 +91,19 @@ pub fn is_utf8(bytes: &[u8]) -> bool {
                 let mut shift1 = _mm_alignr_epi8(input, prev_input, 15);
                 let mut pos = _mm_sub_epi8(shift1, _mm_set1_epi8(0xEFi32 as i8));
                 tmp1 = _mm_subs_epu8(pos, _mm_set1_epi8(240i32 as i8));
-                let mut range2 = _mm_shuffle_epi8(df_ee_tbl, tmp1);
+                let mut range2 = _mm_shuffle_epi8(df_ee_table, tmp1);
                 tmp2 = _mm_adds_epu8(pos, _mm_set1_epi8(112));
-                range2 = _mm_add_epi8(range2, _mm_shuffle_epi8(ef_fe_tbl, tmp2));
+                range2 = _mm_add_epi8(range2, _mm_shuffle_epi8(ef_fe_table, tmp2));
                 range = _mm_add_epi8(range, range2);
-                let mut minv: __m128i = _mm_shuffle_epi8(range_min_tbl, range);
-                let mut maxv: __m128i = _mm_shuffle_epi8(range_max_tbl, range);
+                let mut minv: __m128i = _mm_shuffle_epi8(range_min_table, range);
+                let mut maxv: __m128i = _mm_shuffle_epi8(range_max_table, range);
                 error = _mm_or_si128(error, _mm_cmplt_epi8(input, minv));
                 error = _mm_or_si128(error, _mm_cmpgt_epi8(input, maxv));
                 /* **************************** block 2 ****************************/
-                let _input: __m128i = _mm_lddqu_si128(data.offset(16 as isize) as *const __m128i);
+                let _input = _mm_lddqu_si128(data.offset(16 as isize) as *const __m128i);
                 high_nibbles = _mm_and_si128(_mm_srli_epi16(_input, 4), _mm_set1_epi8(0xF));
-                let mut _first_len: __m128i = _mm_shuffle_epi8(first_len_tbl, high_nibbles);
-                let mut _range: __m128i = _mm_shuffle_epi8(first_range_tbl, high_nibbles);
+                let mut _first_len = _mm_shuffle_epi8(first_len_table, high_nibbles);
+                let mut _range = _mm_shuffle_epi8(first_range_table, high_nibbles);
                 _range = _mm_or_si128(_range, _mm_alignr_epi8(_first_len, first_len, 15));
                 tmp1 = _mm_subs_epu8(_first_len, _mm_set1_epi8(1));
                 tmp2 = _mm_subs_epu8(first_len, _mm_set1_epi8(1));
@@ -112,16 +111,16 @@ pub fn is_utf8(bytes: &[u8]) -> bool {
                 tmp1 = _mm_subs_epu8(_first_len, _mm_set1_epi8(2));
                 tmp2 = _mm_subs_epu8(first_len, _mm_set1_epi8(2));
                 _range = _mm_or_si128(_range, _mm_alignr_epi8(tmp1, tmp2, 13));
-                let mut _range2: __m128i = _mm_setzero_si128();
+                let mut _range2 = _mm_setzero_si128();
                 shift1 = _mm_alignr_epi8(_input, input, 15);
                 pos = _mm_sub_epi8(shift1, _mm_set1_epi8(0xEFi32 as i8));
                 tmp1 = _mm_subs_epu8(pos, _mm_set1_epi8(240i32 as i8));
-                _range2 = _mm_shuffle_epi8(df_ee_tbl, tmp1);
+                _range2 = _mm_shuffle_epi8(df_ee_table, tmp1);
                 tmp2 = _mm_adds_epu8(pos, _mm_set1_epi8(112));
-                _range2 = _mm_add_epi8(_range2, _mm_shuffle_epi8(ef_fe_tbl, tmp2));
+                _range2 = _mm_add_epi8(_range2, _mm_shuffle_epi8(ef_fe_table, tmp2));
                 _range = _mm_add_epi8(_range, _range2);
-                minv = _mm_shuffle_epi8(range_min_tbl, _range);
-                maxv = _mm_shuffle_epi8(range_max_tbl, _range);
+                minv = _mm_shuffle_epi8(range_min_table, _range);
+                maxv = _mm_shuffle_epi8(range_max_table, _range);
                 error = _mm_or_si128(error, _mm_cmplt_epi8(_input, minv));
                 error = _mm_or_si128(error, _mm_cmpgt_epi8(_input, maxv));
                 /* *********************** next iteration **************************/
