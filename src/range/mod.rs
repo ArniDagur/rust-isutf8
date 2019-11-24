d72defdee425e1c23d85202e820ad8d26b70fc88
Árni Dagur <arnidg@protonmail.ch>
2019-11-24 18:06:27 +0000

    Run cargo fmt

--- src/range/mod.rs
+++ src/range/mod.rs
@@ -7,9 +7,9 @@
 //! This module is expertimental. There may be breaking API changes.
 //!
 //! [1]: https://github.com/cyb70289/utf8
-#[cfg(any(all(target_feature = "sse4.1"), dox))]
-#[doc(cfg(all(target_feature = "sse4.1")))]
-pub mod sse;
 #[cfg(any(all(target_feature = "avx", target_feature = "avx2"), dox))]
 #[doc(cfg(all(target_feature = "avx", target_feature = "avx2")))]
 pub mod avx;
+#[cfg(any(all(target_feature = "sse4.1"), dox))]
+#[doc(cfg(all(target_feature = "sse4.1")))]
+pub mod sse;
