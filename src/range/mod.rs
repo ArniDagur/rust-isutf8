571b4710a674bfad40bebd5f950e2e5414b82c70
Árni Dagur <arni@dagur.eu>
2019-11-24 17:19:59 +0000

    Add unstability warning

--- src/range/mod.rs
+++ src/range/mod.rs
@@ -3,6 +3,9 @@
 //! The algoritm's implementation is adapted from Yibo Cai's UTF-8 library
 //! which can be found [on GitHub][1].
 //!
+//! ## Warning:
+//! This module is expertimental. There may be breaking API changes.
+//!
 //! [1]: https://github.com/cyb70289/utf8
 #[cfg(any(all(target_feature = "sse4.1"), dox))]
 #[doc(cfg(all(target_feature = "sse4.1")))]
